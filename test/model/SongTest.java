36bfe02162e195cf76997328ba9837045f6c4ec2
IsaacCotaJr <166754687+IsaacCotaJr@users.noreply.github.com>
2025-03-20 11:58:17 -0700

    Update tests for updated methods

--- test/model/SongTest.java
+++ test/model/SongTest.java
@@ -7,7 +7,7 @@ import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
 
 class SongTest {
-	private Song song1 = new Song("one", "Joe Biden", "yes");
+	private Song song1 = new Song("one", "Joe Biden", "yes", "dem");
 	
 	@Test
 	void getTitleTest() {
@@ -22,6 +22,11 @@ class SongTest {
 	@Test
 	void getAlbumTest() {
 		assertEquals(song1.getAlbum(), "yes");
+	}
+	
+	@Test
+	void getGenreTest() {
+		assertEquals(song1.getGenre(), "dem");
 	}
 	
 	@Test
@@ -44,25 +49,25 @@ class SongTest {
 	
 	@Test
 	void equalToFalseTest() {
-		Song song2 = new Song("two", "Donald Trump", "no");
+		Song song2 = new Song("two", "Donald Trump", "no", "rep");
 		assertFalse(song1.equalTo(song2));
 	}
 	
 	@Test
 	void equalToTrueTest() {
-		Song song2 = new Song("one", "Joe Biden", "yes");
+		Song song2 = new Song("one", "Joe Biden", "yes", "dem");
 		assertTrue(song1.equalTo(song2));
 	}
 	
 	@Test
 	void equalToDifferentArtistTest() {
-		Song song2 = new Song("one", "Donald Trump", "yes");
+		Song song2 = new Song("one", "Donald Trump", "yes", "dem");
 		assertFalse(song1.equalTo(song2));
 	}
 	
 	@Test
 	void equalToDifferentAlbumTest() {
-		Song song2 = new Song("one", "Joe Biden", "no");
+		Song song2 = new Song("one", "Joe Biden", "no", "dem");
 		assertFalse(song1.equalTo(song2));
 	}
 	
@@ -76,6 +81,14 @@ class SongTest {
 	void rateSongNotFavorite() {
         song1.rateSong(1);
         assertFalse(song1.getFavorite());
+	}
+	
+	@Test
+	void playandGetPlays() {
+		song1.play();
+		assertEquals(song1.getPlays(), 1);
+		song1.play();
+		assertEquals(song1.getPlays(), 2);
 	}
 	
 	@Test
